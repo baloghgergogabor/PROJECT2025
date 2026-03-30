af81306f55a33cbf9e307fc302a9b86df7fd6696
baloghgergogabor <balogh.gergo.gabor@ganziskola.hu>
2026-03-30 09:34:11 +0200

    hash javtítás

--- PROJECT_2025/TeUtad.API/obj/Debug/net8.0/TeUtad.API.AssemblyInfo.cs
+++ PROJECT_2025/TeUtad.API/obj/Debug/net8.0/TeUtad.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeUtad.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb36c6abbe96359258078ef180ec7680da91d3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141f006f699e93c481046661b384860118627013")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeUtad.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeUtad.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PROJECT_2025/TeUtad.LIB/obj/Debug/net8.0/TeUtad.LIB.AssemblyInfo.cs
+++ PROJECT_2025/TeUtad.LIB/obj/Debug/net8.0/TeUtad.LIB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeUtad.LIB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb36c6abbe96359258078ef180ec7680da91d3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141f006f699e93c481046661b384860118627013")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeUtad.LIB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeUtad.LIB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
